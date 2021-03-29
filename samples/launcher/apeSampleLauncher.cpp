99ca118087867fab3047eb6a9053fd6287f151b5
Akos Hamori <hamoriakos@gmail.com>
2021-03-29 17:38:44 +0200

    rework apeSampleLauncher
    Signed-off-by: Akos Hamori <hamoriakos@gmail.com>

--- samples/launcher/apeSampleLauncher.cpp
+++ samples/launcher/apeSampleLauncher.cpp
@@ -20,36 +20,58 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.*/
 
+#include "apeSystem.h"
 #include <iostream>
 #include <signal.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <thread>
-#include "apeSystem.h"
 
-bool blockingMode = true;
+bool shouldStop = false;
 
-void my_handler(int s) {
-	std::cout << "Caught signal " << s << ", quitting..." << std::endl;
+void stopHandlerBlocking(int s)
+{
+	std::cout << "stopHandlerBlocking() Caught signal " << s << ", quitting..." << std::endl;
 	ape::System::Stop();
-	exit(1);
+	exit(0);
+}
+
+void stopHandlerNonBlocking(int s)
+{
+	std::cout << "stopHandlerNonBlocking() Caught signal " << s << ", quitting..." << std::endl;
+	ape::System::Stop();
+	shouldStop = true;
+}
+
+void blockingMode(const char* configFilePath)
+{
+	std::cout << "Starting in blocking mode" << std::endl;
+	signal(SIGINT, stopHandlerBlocking);
+	ape::System::Start(configFilePath, true); // interrupts main thread when joining plugin threads
+	ape::System::Stop(); // only called if stopHandlerBlocking() is not called
+}
+
+void nonBlockingMode(const char* configFilePath)
+{
+	std::cout << "Starting in non-blocking mode" << std::endl;
+	signal(SIGINT, stopHandlerNonBlocking);
+	ape::System::Start(configFilePath, false);
+	while (!shouldStop)
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 }
 
 int main(int argc, char** argv)
 {
-	if (argc > 1)
+	if (argc < 2 || argc > 3)
 	{
-		signal(SIGINT, my_handler);
-		ape::System::Start(argv[1], blockingMode);
-		if (!blockingMode)
-		{
-			while (true)
-				std::this_thread::sleep_for(std::chrono::milliseconds(20));
-		}
+		std::cout << "Usage example: apeSampleLauncher configFilePath:string [blockingmode:bool - optional, default: false]" << std::endl;
+		exit(1);
 	}
+
+	if (argc == 2 || (argc == 3 && strcmp(argv[2], "false") == 0))
+		nonBlockingMode(argv[1]);
 	else
-	{
-		std::cout << "Usage example: apeSampleLauncher configFilePath" << std::endl;
-	}
+		blockingMode(argv[1]);
+
 	return 0;
 }
